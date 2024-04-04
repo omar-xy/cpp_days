c098609b87681bc1fb4659ab55639f62dcb7c484
omar-xy <ortaraki@gmail.com>
2024-04-04 01:57:13 +0000

    Done

---
+++ cpp_08/ex02/MutantStack.hpp
@@ -0,0 +1,32 @@
+# pragma once
+
+
+#include <stack>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <list>
+
+
+template <typename T, class Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container> {
+public:
+    typedef typename Container::iterator iterator;
+    typedef typename Container::const_iterator const_iterator;
+
+    iterator begin() {
+        return this->c.begin();
+    }
+
+    iterator end() {
+        return this->c.end();
+    }
+
+    const_iterator begin() const {
+        return this->c.begin();
+    }
+
+    const_iterator end() const {
+        return this->c.end();
+    }
+};
